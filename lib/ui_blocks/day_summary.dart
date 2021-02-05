46e22e3d11a3bd73f900d6bfb25723b3408ad323
Goran Alković <goran.alkovic@hotmail.com>
2021-02-06 00:42:01 +0100

    remove unused import

--- lib/ui_blocks/day_summary.dart
+++ lib/ui_blocks/day_summary.dart
@@ -1,6 +1,5 @@
 import 'package:covid19hr/components/day_case_ratios.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:provider/provider.dart';
 
 import '../appstate.dart';
