ca8bc6dca59d1419774985100a49cd11ae3c50cf
Goran Alković <goran.alkovic@hotmail.com>
2021-02-06 00:26:02 +0100

    refactor TableView

--- lib/table_view.dart
+++ lib/table_view.dart
@@ -1,5 +1,4 @@
-import 'dart:math';
-
+import 'package:covid19hr/model.dart';
 import 'package:covid19hr/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -9,230 +8,270 @@ import 'package:shimmer_animation/shimmer_animation.dart';
 
 import 'appstate.dart';
 
+class TableViewSource extends DataTableSource {
+  final List<GlobalDataRecord> data;
+  final BuildContext context;
+
+  TableViewSource(this.data, this.context);
+
+  @override
+  DataRow getRow(int index) {
+    // TODO: implement getRow
+    return getTableViewRow(
+      data[index],
+      data.first.date,
+      context,
+      index: data.length - index,
+    );
+  }
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get rowCount => data.length;
+
+  @override
+  int get selectedRowCount => 0;
+}
+
 class TableView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final provider = context.watch<Covid19Provider>();
 
     final data = provider.data;
-    double chartHeight = 400;
 
-    return SizedBox(
-      // height: 140 + chartHeight,
-      child: Column(
-        children: [
-          Container(
-            height: 40,
-            decoration: BoxDecoration(
-              border: Border(
-                bottom: BorderSide(
-                  color: Colors.grey.withOpacity(0.6),
-                ),
-              ),
+    // final firstDate = provider.globalRecords.first.date;
+
+    NumberFormat formatter = NumberFormat('###,###,###');
+
+    return AnimatedCrossFade(
+      crossFadeState: provider.loading || provider.data.isEmpty
+          ? CrossFadeState.showFirst
+          : CrossFadeState.showSecond,
+      duration: Duration(milliseconds: 300),
+      firstChild: Shimmer(
+        child: SizedBox(height: 320, width: double.infinity),
+      ),
+      secondChild: ListView.builder(
+        padding: const EdgeInsets.all(0),
+        primary: false,
+        itemCount: data.length,
+        shrinkWrap: true,
+        reverse: false,
+        physics: BouncingScrollPhysics(),
+        itemBuilder: (context, index) {
+          final dataItem = data.reversed.toList()[index];
+
+          return Container(
+            color: index % 2 == 0
+                ? Theme.of(context).dividerColor.withOpacity(0.02)
+                : Colors.transparent,
+            padding: const EdgeInsets.symmetric(
+              vertical: 8.0,
+              horizontal: 18.0,
             ),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 SizedBox(
-                  width: 48,
-                  child: Text('Datum', style: tableHeader),
+                  width: 68,
+                  child: Text(
+                    DateFormat("d. M.").format(dataItem.date),
+                    style: tableItem,
+                  ),
                 ),
                 SizedBox(
-                  width: 48,
-                  child: Text('Ukupno', style: tableHeader),
+                  width: 84,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        formatter.format(dataItem.totalCases),
+                        style: tableItem.copyWith(fontSize: 15),
+                      ),
+                      Text(
+                        dataItem.deltaTotalDisplay,
+                        style: tableItemFooter,
+                      ),
+                    ],
+                  ),
                 ),
                 SizedBox(
-                  width: 70,
-                  child: Text('Oporavljeni', style: tableHeader),
+                  width: 84,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        formatter.format(dataItem.recoveries),
+                        style: tableItem,
+                      ),
+                      Text(
+                        dataItem.deltaRecoveriesDisplay,
+                        style: tableItemFooter,
+                      ),
+                    ],
+                  ),
                 ),
                 SizedBox(
-                  width: 48,
-                  child: Text('Umrli', style: tableHeader),
+                  width: 84,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        formatter.format(dataItem.activeCases),
+                        style: tableItem,
+                      ),
+                      Text(
+                        dataItem.deltaActiveDisplay,
+                        style: tableItemFooter,
+                      ),
+                    ],
+                  ),
                 ),
                 SizedBox(
-                  width: 48,
-                  child: Text('Aktivni', style: tableHeader),
+                  width: 84,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        formatter.format(dataItem.deaths),
+                        style: tableItem,
+                      ),
+                      Text(
+                        dataItem.deltaDeathsDisplay,
+                        style: tableItemFooter,
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
-          ),
-          AnimatedCrossFade(
-            crossFadeState: provider.loading || provider.data.isEmpty
-                ? CrossFadeState.showFirst
-                : CrossFadeState.showSecond,
-            duration: Duration(milliseconds: 300),
-            firstChild: Shimmer(
-              child: SizedBox(height: 320, width: double.infinity),
-            ),
-            secondChild: SizedBox(
-              height: 100 + chartHeight,
-              child: ListView.builder(
-                padding: const EdgeInsets.all(0),
-                primary: false,
-                itemCount: data.length,
-                shrinkWrap: true,
-                reverse: false,
-                physics: BouncingScrollPhysics(),
-                itemBuilder: (context, index) {
-                  final dataItem = data.reversed.toList()[index];
-                  final f2 = DateFormat('d. M.');
-
-                  String deltaTotal = '';
-                  String deltaDeaths = '';
-                  String deltaRecoveries = '';
-                  String deltaActive = '';
-
-                  deltaTotal = dataItem.deltaTotalDisplay;
-                  deltaActive = dataItem.deltaActiveDisplay;
-                  deltaDeaths = dataItem.deltaDeathsDisplay;
-                  deltaRecoveries = dataItem.deltaRecoveriesDisplay;
-
-                  if (deltaTotal == '0') deltaTotal = '—';
-                  if (deltaRecoveries == '0') deltaRecoveries = '—';
-                  if (deltaDeaths == '0') deltaDeaths = '—';
-                  if (deltaActive == '0') deltaActive = '—';
-
-                  final isToday = index == 0;
-
-                  int dayDiff = dataItem.date
-                          .difference(provider.globalRecords.first.date)
-                          .inDays +
-                      1;
-
-                  return Container(
-                    height: 56,
-                    margin: const EdgeInsets.symmetric(
-                      vertical: 2,
-                      horizontal: 0,
-                    ),
-                    decoration: BoxDecoration(
-                      color: index % 2 == 0
-                          ? Colors.grey.withOpacity(0.05)
-                          : Colors.transparent,
-                      borderRadius: BorderRadius.circular(6),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Container(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                f2.format(dataItem.date),
-                                style: isToday
-                                    ? tableItem.copyWith(
-                                        color: Theme.of(context).accentColor)
-                                    : tableItem,
-                              ),
-                              Opacity(
-                                opacity: 0.6,
-                                child: Text(
-                                  'dan $dayDiff',
-                                  style: tableItemFooter.copyWith(fontSize: 12),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Container(
-                          alignment: AlignmentDirectional.centerStart,
-                          width: 48,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                '${dataItem.totalCases}',
-                                style: isToday
-                                    ? tableItem.copyWith(
-                                        fontWeight: FontWeight.w500,
-                                        color: Theme.of(context).accentColor,
-                                      )
-                                    : tableItem.copyWith(
-                                        fontWeight: FontWeight.w500,
-                                      ),
-                              ),
-                              Text(
-                                '$deltaTotal',
-                                style: tableItemFooter,
-                              ),
-                            ],
-                          ),
-                        ),
-                        Container(
-                          alignment: AlignmentDirectional.centerStart,
-                          width: 70,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                '${dataItem.recoveries}',
-                                style: isToday
-                                    ? tableItem.copyWith(
-                                        color: Theme.of(context).accentColor)
-                                    : tableItem,
-                              ),
-                              Text(
-                                '$deltaRecoveries',
-                                style: tableItemFooter,
-                              ),
-                            ],
-                          ),
-                        ),
-                        Container(
-                          alignment: AlignmentDirectional.centerStart,
-                          width: 48,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                '${dataItem.deaths}',
-                                style: isToday
-                                    ? tableItem.copyWith(
-                                        color: Theme.of(context).accentColor)
-                                    : tableItem,
-                              ),
-                              Text(
-                                '$deltaDeaths',
-                                style: tableItemFooter,
-                              ),
-                            ],
-                          ),
-                        ),
-                        Container(
-                          alignment: AlignmentDirectional.centerStart,
-                          width: 48,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                '${dataItem.activeCases}',
-                                style: isToday
-                                    ? tableItem.copyWith(
-                                        color: Theme.of(context).accentColor)
-                                    : tableItem,
-                              ),
-                              Text(
-                                '$deltaActive',
-                                style: tableItemFooter,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
+}
+
+DataRow getTableViewRow(
+    GlobalDataRecord dI, DateTime firstDate, BuildContext context,
+    {int index}) {
+  final dataItem = GenericDataRecord.fromGlobal(dI);
+  final f2 = DateFormat('d. M.');
+
+  String deltaTotal = '';
+  String deltaDeaths = '';
+  String deltaRecoveries = '';
+  String deltaActive = '';
+
+  deltaTotal = dataItem.deltaTotalDisplay;
+  deltaActive = dataItem.deltaActiveDisplay;
+  deltaDeaths = dataItem.deltaDeathsDisplay;
+  deltaRecoveries = dataItem.deltaRecoveriesDisplay;
+
+  if (deltaTotal == '0') deltaTotal = '—';
+  if (deltaRecoveries == '0') deltaRecoveries = '—';
+  if (deltaDeaths == '0') deltaDeaths = '—';
+  if (deltaActive == '0') deltaActive = '—';
+
+  final isToday = false; //index == 0;
+
+  int dayDiff = dataItem.date.difference(firstDate).inDays + 1;
+
+  return DataRow(
+    cells: [
+      DataCell(
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              f2.format(dataItem.date),
+              style: isToday
+                  ? tableItem.copyWith(color: Theme.of(context).accentColor)
+                  : tableItem,
+            ),
+            Opacity(
+              opacity: 0.6,
+              child: Text(
+                'dan ${index ?? dayDiff}',
+                style: tableItemFooter.copyWith(fontSize: 12),
+              ),
+            ),
+          ],
+        ),
+      ),
+      DataCell(Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '${dataItem.totalCases}',
+            style: isToday
+                ? tableItem.copyWith(
+                    fontWeight: FontWeight.w500,
+                    color: Theme.of(context).accentColor,
+                  )
+                : tableItem.copyWith(
+                    fontWeight: FontWeight.w500,
+                  ),
+          ),
+          Text(
+            '$deltaTotal',
+            style: tableItemFooter,
+          ),
+        ],
+      )),
+      DataCell(Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '${dataItem.recoveries}',
+            style: isToday
+                ? tableItem.copyWith(color: Theme.of(context).accentColor)
+                : tableItem,
+          ),
+          Text(
+            '$deltaRecoveries',
+            style: tableItemFooter,
+          ),
+        ],
+      )),
+      DataCell(Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '${dataItem.deaths}',
+            style: isToday
+                ? tableItem.copyWith(color: Theme.of(context).accentColor)
+                : tableItem,
+          ),
+          Text(
+            '$deltaDeaths',
+            style: tableItemFooter,
+          ),
+        ],
+      )),
+      DataCell(Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '${dataItem.activeCases}',
+            style: isToday
+                ? tableItem.copyWith(color: Theme.of(context).accentColor)
+                : tableItem,
+          ),
+          Text(
+            '$deltaActive',
+            style: tableItemFooter,
+          ),
+        ],
+      )),
+    ],
+  );
 }
